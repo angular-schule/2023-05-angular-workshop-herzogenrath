626fe96cffce0236f15213c8ea91bd3a68a85cd6
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2023-05-04 15:13:52 +0200

    fertig! :D

--- book-rating/src/app/dashboard/dashboard.component.ts
+++ book-rating/src/app/dashboard/dashboard.component.ts
@@ -32,4 +32,20 @@ export class DashboardComponent {
     description: 'Super altes Buch',
     rating: 1
   }];
+
+  doRateUp(book: Book) {
+    const ratedBook = this.br.rateUp(book);
+    this.updateAndSort(ratedBook);
+  }
+
+  doRateDown(book: Book) {
+    const ratedBook = this.br.rateDown(book);
+    this.updateAndSort(ratedBook);
+  }
+
+  updateAndSort(ratedBook: Book) {
+    this.books = this.books
+      .map(b => b.isbn === ratedBook.isbn ? ratedBook : b)
+      .sort((a, b) => b.rating - a.rating)
+  }
 }
