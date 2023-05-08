857984156e71ce819007a537e7ba4d0680186143
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2023-05-08 11:03:55 +0200

    Forms: Hands on!

--- book-rating/src/app/book-create/book-create.component.ts
+++ book-rating/src/app/book-create/book-create.component.ts
@@ -1,10 +1,12 @@
+import { NgIf } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Book } from '../shared/book';
 
 @Component({
   selector: 'app-book-create',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [NgIf,ReactiveFormsModule],
   templateUrl: './book-create.component.html',
   styleUrls: ['./book-create.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -26,5 +28,31 @@ export class BookCreateComponent {
     description: new FormControl('', {
       nonNullable: true
     })
-  })
+  });
+
+  c = this.bookForm.controls;
+
+  isInvalid(control: FormControl) {
+    return control.invalid && control.touched;
+  }
+
+  hasError(control: FormControl, errorCode: string) {
+    return control.invalid &&
+      control.touched &&
+      control.hasError(errorCode);
+  }
+
+  submitForm() {
+    const newBook: Book = {
+      ...this.bookForm.getRawValue(),
+      rating: 1
+    }
+
+    // 1. Erstelle eine Ereignis mit dem Namen create (Payload Buch)
+    // 2. Reiche das Buch weiter
+    // 3. Abonniere dich auf das Ereignis in Dashboard
+    // 4. Füge das Buch dem Array hinzu
+
+    this.bookForm.reset();
+  }
 }
