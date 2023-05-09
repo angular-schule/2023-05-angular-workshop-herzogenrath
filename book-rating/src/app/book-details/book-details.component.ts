4cd692600b0c69c145b99ebee3c35ce54e04bbba
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2023-05-09 09:27:56 +0200

    routing: with snapshot 🙁

--- book-rating/src/app/book-details/book-details.component.ts
+++ book-rating/src/app/book-details/book-details.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-book-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './book-details.component.html',
   styleUrls: ['./book-details.component.scss']
 })
 export default class BookDetailsComponent {
 
+  isbn = inject(ActivatedRoute).snapshot.paramMap.get('isbn')
 }
