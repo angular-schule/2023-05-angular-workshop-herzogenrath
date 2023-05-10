eac3d0f3e393679f037663b0868457b2090a1404
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2023-05-10 12:00:21 +0200

    ngrx: adding feature to app.config.ts

--- book-rating/src/app/app.config.ts
+++ book-rating/src/app/app.config.ts
@@ -4,9 +4,11 @@ import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { ApiModule, Configuration } from './shared/http';
-import { provideStore } from '@ngrx/store';
+import { provideStore, provideState } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { bookFeature } from './store/book.reducer';
+import { BookEffects } from './store/book.effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -16,7 +18,11 @@ export const appConfig: ApplicationConfig = {
         basePath: 'https://api.angular.schule',
     }))),
     provideStore(),
-    provideEffects(),
+    // provideEffects(),
+
+    provideState(bookFeature),
+    provideEffects(BookEffects),
+
     provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
 ]
 };
