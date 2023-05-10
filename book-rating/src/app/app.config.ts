e1cd43637268e8e7ce643062f4f40343dc5f1fb9
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2023-05-10 11:42:36 +0200

    ng add @ngrx/store ng add @ngrx/effects ng add @ngrx/store-devtools ng add @ngrx/schematics

--- book-rating/src/app/app.config.ts
+++ book-rating/src/app/app.config.ts
@@ -1,21 +1,22 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, isDevMode } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { ApiModule, Configuration } from './shared/http';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes, withComponentInputBinding()),
-
     provideHttpClient(),
-    importProvidersFrom(
-      ApiModule.forRoot(
-        () => new Configuration({
-          basePath: 'https://api.angular.schule',
-        })
-      )
-    )
-  ]
+    importProvidersFrom(ApiModule.forRoot(() => new Configuration({
+        basePath: 'https://api.angular.schule',
+    }))),
+    provideStore(),
+    provideEffects(),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
+]
 };
