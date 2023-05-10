66df6768bac3f14ce99d1a358858a44e14b3b3a5
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2023-05-10 10:49:25 +0200

    rxjs: share()

--- book-rating/src/app/book-details/book-details.component.ts
+++ book-rating/src/app/book-details/book-details.component.ts
@@ -1,22 +1,24 @@
 import { Component, Input, inject } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { AsyncPipe, CommonModule, JsonPipe } from '@angular/common';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { Book, BooksslowService } from '../shared/http';
+import { concatMap, map, mergeMap, share, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-book-details',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [RouterLink, AsyncPipe, JsonPipe],
   templateUrl: './book-details.component.html',
   styleUrls: ['./book-details.component.scss']
 })
 export default class BookDetailsComponent {
 
   bs = inject(BooksslowService);
-  book?: Book;
 
-  @Input()
-  public set isbn(value: string) {
-    this.bs.booksIsbnSlowGet(value).subscribe(b => this.book = b)
-  }
+  book$ = inject(ActivatedRoute).paramMap.pipe(
+    map(paramMap => paramMap.get('isbn') || ''),
+    switchMap(isbn => this.bs.booksIsbnSlowGet(isbn)),
+    share()
+  );
+
 }
