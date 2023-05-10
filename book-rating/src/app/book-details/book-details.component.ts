09cda0b8cc30dc79180a87ec9500a6876596df57
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2023-05-10 10:53:54 +0200

    rxjs: problem mit normalem Subject

--- book-rating/src/app/book-details/book-details.component.ts
+++ book-rating/src/app/book-details/book-details.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, inject } from '@angular/core';
-import { AsyncPipe, CommonModule, JsonPipe } from '@angular/common';
+import { AsyncPipe, CommonModule, JsonPipe, NgIf } from '@angular/common';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { Book, BooksslowService } from '../shared/http';
 import { concatMap, map, mergeMap, share, switchMap } from 'rxjs/operators';
@@ -7,13 +7,14 @@ import { concatMap, map, mergeMap, share, switchMap } from 'rxjs/operators';
 @Component({
   selector: 'app-book-details',
   standalone: true,
-  imports: [RouterLink, AsyncPipe, JsonPipe],
+  imports: [RouterLink, AsyncPipe, JsonPipe, NgIf],
   templateUrl: './book-details.component.html',
   styleUrls: ['./book-details.component.scss']
 })
 export default class BookDetailsComponent {
 
   bs = inject(BooksslowService);
+  showDetails = false;
 
   book$ = inject(ActivatedRoute).paramMap.pipe(
     map(paramMap => paramMap.get('isbn') || ''),
