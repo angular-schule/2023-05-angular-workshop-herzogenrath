dc71c33145c9f022f1cac3c8bcf952c09b52aa73
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2023-05-10 10:10:32 +0200

    rxjs + signals

--- rxjs-playground/src/app/exercises/unsubscribe/unsubscribe.component.ts
+++ rxjs-playground/src/app/exercises/unsubscribe/unsubscribe.component.ts
@@ -1,19 +1,33 @@
 import { ChangeDetectionStrategy, Component, OnDestroy } from '@angular/core';
-import { Subject, ReplaySubject, timer, Subscription, takeWhile, takeUntil, tap } from 'rxjs';
+import { Subject, ReplaySubject, timer, Subscription, takeWhile, takeUntil, tap, delay } from 'rxjs';
 import { HistoryComponent } from '../../shared/history/history.component';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { AsyncPipe } from '@angular/common';
+import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
+import { AsyncPipe, JsonPipe } from '@angular/common';
 
 
 @Component({
   templateUrl: './unsubscribe.component.html',
   standalone: true,
-  imports: [HistoryComponent, AsyncPipe],
+  imports: [HistoryComponent, AsyncPipe, JsonPipe],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class UnsubscribeComponent {
 
   interval$ = timer(0, 2000).pipe(
-    tap(console.log)
-  )
+    delay(4000),
+    // tap(console.log)
+  );
+
+  interval$$ = toSignal(timer(0, 2000).pipe(
+    delay(4000),
+    // tap(console.log)
+  ));
+
+  getZahl() {
+    // Mit Observables
+    this.interval$.subscribe(console.log)
+
+    // Mit Signal
+    console.log(this.interval$$())
+  }
 }
